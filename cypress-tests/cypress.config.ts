dc53cc393baace2636178ecc79bbce96154ccf4e
Lukáš Jirsa <lukas.jirsa1@gmail.com>
2025-06-05 19:40:44 +0200

    pushConfigCypressYML

--- cypress-tests/cypress.config.ts
+++ cypress-tests/cypress.config.ts
@@ -4,10 +4,12 @@ export default defineConfig({
   viewportHeight: 800,
   viewportWidth: 1280,
   watchForFileChanges: false,
+  projectId: "gcjszx",
   
   
   e2e: {
     baseUrl: 'https://www.spokojenypes.cz',
+    
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
